b15df8dcf9a51241bf2e3f48ee7d772912a9a371
Mirela Damyanova <mdamyanova181@gmail.com>
2019-04-09 14:26:41 +0300

    Add Demo Java app

---
+++ Demo/src/main/java/UsersServlet.java
@@ -0,0 +1,17 @@
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet("/users")
+public class UsersServlet extends HttpServlet {
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        response.setContentType("text/html");
+        PrintWriter out = response.getWriter();
+        out.println("<h3>Hello, user!</h3>");
+    }
+}
+
